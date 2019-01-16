642e65cf913e962699aaf98aae0210743dd41d43
Kadeem Palacios <kadeempalacios@gmail.com>
2019-01-15 23:51:38 -0800

    Project 1 created

---
+++ HWS-Project1/HWS-Project1/ViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Kadeem Palacios on 1/14/19.
+//  Copyright © 2019 Kadeem Palacios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    var pictures = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+
+        print(items)
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
+    }
+
+    @IBOutlet var picturesTableView: UITableView!
+
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = picturesTableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            vc.selectedImage = pictures[indexPath.row]
+            navigationController?.pushViewController(vc, animated: true)
+
+
+
+
+        }
+
+
+
+    }
+}
+
