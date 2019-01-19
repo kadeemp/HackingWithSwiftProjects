55a927a9425c9ee97c8d37fc33c355be3847373f
Kadeem Palacios <kadeempalacios@gmail.com>
2019-01-18 22:56:28 -0800

    Bugs fixed in project 2. Created project 2

---
+++ HWS-Project 4/HWS-Project 4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HWS-Project 4
+//
+//  Created by Kadeem Palacios on 1/18/19.
+//  Copyright © 2019 Kadeem Palacios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
